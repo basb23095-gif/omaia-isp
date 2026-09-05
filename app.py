412cf39fa733b164823c7b32326f2b3dff1f6f5d
omaia isp <basb23095@gmail.com>
2026-09-05 03:15:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def init():
         try:cur.execute("ALTER TABLE users ADD COLUMN IF NOT EXISTS username TEXT")
         except:pass
        cur.execute("SELECT * FROM users WHERE phone='05344851045'")
-if not cur.fetchone():cur.execute("INSERT INTO users(phone,password,role,active) VALUES('05344851045','admin2024','super',1)")
+        if not cur.fetchone():cur.execute("INSERT INTO users(phone,password,role,active) VALUES('05344851045','admin2024','super',1)")
         con.commit();cur.close();return
     con.execute("CREATE TABLE IF NOT EXISTS users(phone TEXT PRIMARY KEY,password TEXT,role TEXT,active INT DEFAULT 1)")
     con.execute("CREATE TABLE IF NOT EXISTS subs(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,phone TEXT,speed TEXT,status TEXT,server_id INT,dish_ip TEXT)")
